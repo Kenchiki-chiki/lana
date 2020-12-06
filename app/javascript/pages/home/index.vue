88b38e5094a36cfde1b5c37360d7fcbbd5ac8800
Matsushin <matsushin.dev@gmail.com>
2020-12-06 19:14:46 +0900

    Vueを導入途中まで

---
+++ app/javascript/pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-btn>Vuetifyのボタン</v-btn>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    }
+  }
+}
+</script>
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+const { resolve } = require('path')
 
+environment.config.merge({
+    resolve: {
+        alias: {
+            '@': resolve('app/javascript')
+        }
+    }
+})
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
