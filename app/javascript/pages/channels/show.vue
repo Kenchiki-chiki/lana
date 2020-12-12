6f74d0c9349cfc4c02d2f7b44b07c460edfd449c
Matsushin <matsushin.dev@gmail.com>
2020-12-12 11:29:54 +0900

    チャンネル一覧が表示するように

---
+++ app/javascript/pages/channels/show.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>チャンネル詳細</h2>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item-group v-for="list_channel in channels" :key="list_channel.id">
+          <a :href="`/channels/${list_channel.id}`">
+            <v-list-item>
+              <v-list-item-title>
+                {{ list_channel.name }}
+              </v-list-item-title>
+            </v-list-item>
+          </a>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    channels: {
+      type: Array
+    },
+    channel: {
+      type: Object
+    }
+  }
+}
+</script>
