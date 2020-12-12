71d7b243a9e4f490216ee3fd69afdf2d022e5896
Kenchiki-chiki <kodama303@gmail.com>
2020-12-12 14:11:31 +0900

    ユーザー名が保存されるように修正

---
+++ app/javascript/components/profile_icon.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-app class="account_icon">
+      <v-icon @click="drawer = true" color="indigo">
+      mdi-account-circle
+      </v-icon>
+      <v-menu
+        v-model="drawer"
+      >
+        <v-list
+          class="list_position"
+          nav
+          dense
+        >
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-title
+               class="text--darken-3 mb-1"
+               @click="logout"
+              >
+              ログアウト
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+          
+        </v-list>
+      </v-menu>
+    </v-app>
+</template>
+
+
+
+
